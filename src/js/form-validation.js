dc86648cbe57730d98f974ebc75adaacc198e31f
webkey <serhiy_i@ukr.net>
2018-08-11 14:28:34 +0300

    20180811_1425

--- src/js/form-validation.js
+++ src/js/form-validation.js
@@ -13,39 +13,42 @@ $(function () {
 	var $validationForm = $('.validation-form-js');
 
 	if($validationForm.length) {
-		$validationForm.validate({
-			errorClass: 'error',
-			validClass: 'success',
-			errorElement: false,
-			errorPlacement: function(error,element) {
-				return true;
-			},
-			highlight: function(element, errorClass, successClass) {
-				$(element)
-					.removeClass(successClass)
-					.addClass(errorClass);
-				$(element)
-					.closest('form').find('label[for="' + $(element).attr('id') + '"]')
-					.removeClass(successClass)
-					.addClass(errorClass);
-				$(element)
-					.closest('.input-holder')
-					.removeClass(successClass)
-					.addClass(errorClass);
-			},
-			unhighlight: function(element, errorClass, successClass) {
-				$(element)
-					.removeClass(errorClass)
-					.addClass(successClass);
-				$(element)
-					.closest('form').find('label[for="' + $(element).attr('id') + '"]')
-					.removeClass(errorClass)
-					.addClass(successClass);
-				$(element)
-					.closest('.input-holder')
-					.removeClass(errorClass)
-					.addClass(successClass);
-			}
+		$.each($validationForm, function (i, el) {
+			console.log("el: ", el);
+			$(el).validate({
+				errorClass: 'error',
+				validClass: 'success',
+				errorElement: false,
+				errorPlacement: function(error,element) {
+					return true;
+				},
+				highlight: function(element, errorClass, successClass) {
+					$(element)
+						.removeClass(successClass)
+						.addClass(errorClass);
+					$(element)
+						.closest('form').find('label[for="' + $(element).attr('id') + '"]')
+						.removeClass(successClass)
+						.addClass(errorClass);
+					$(element)
+						.closest('.input-holder')
+						.removeClass(successClass)
+						.addClass(errorClass);
+				},
+				unhighlight: function(element, errorClass, successClass) {
+					$(element)
+						.removeClass(errorClass)
+						.addClass(successClass);
+					$(element)
+						.closest('form').find('label[for="' + $(element).attr('id') + '"]')
+						.removeClass(errorClass)
+						.addClass(successClass);
+					$(element)
+						.closest('.input-holder')
+						.removeClass(errorClass)
+						.addClass(successClass);
+				}
+			});
 		});
 	}
 });
